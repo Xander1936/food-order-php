48dfecb3c231a53e026dd834fe367f54c592752c
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2025-07-04 19:33:21 +0100

    Start Login System

---
+++ admin/login.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="../css/admin.css">
+        <title>Login - Food Order System</title>
+    </head>
+
+    <body>
+        <div class="login">
+            <h1 class="text-center">Login</h1>
+
+            <!-- Login Form Starts Here -->
+                    
+            <!-- Login Form Ends Here -->
+
+            <p class="text-center">Created By - <a href="https://www.linkedin.com/in/alexandre-massoda">Alexandre Massoda</a> </p>
+        </div>
+    </body>
+</html>
+
+<?php include("partials/footer.php"); ?>

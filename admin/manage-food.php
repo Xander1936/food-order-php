2768fce91c4ed18f021367212d2637247d1b9895
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2025-07-26 04:19:37 +0100

    Inserting Food of Database into our Managing Food System

--- admin/manage-food.php
+++ admin/manage-food.php
@@ -5,7 +5,7 @@
         <h1>Manage Food</h1>
 
         <br><br>
-                <!-- Button to Add Admin -->
+                <!-- Button to Add Food -->
                  <a href="<?php echo SITEURL; ?>admin/add-food.php" class="btn-primary">Add Food</a>
                  
                  <br><br><br>
@@ -18,44 +18,86 @@
                     }
                  ?>
 
-                 <table class="tbl-full">
+                <table class="tbl-full">
                      <tr>
                          <th>S.N.</th>
-                         <th>Full Name</th>
-                         <th>Username</th>
+                         <th>Title</th>
+                         <th>Price</th>
+                         <th>Image</th>
+                         <th>Featured</th>
+                         <th>Active</th>
                          <th>Actions</th>
                         </tr>
-                        
-                        <tr>
-                            <td>1.</td>
-                            <td>Vijay Thapa</td>
-                            <td>vijaythapa</td>
-                            <td>
-                            <a href="#" class="btn-secondary">Update Food</a>
-                            <a href="#" class="btn-danger">Delete Food</a>
-                        </td>
-                    </tr>
 
-                    <tr>
-                        <td>2.</td>
-                        <td>Alexandre Massoda</td>
-                        <td>alex_xander</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Food</a>
-                            <a href="#" class="btn-danger">Delete Food</a>
-                        </td>
-                    </tr>
+                        <?php 
+                            // Create a SQL Query to Get all the Food
+                            $sql = "SELECT * FROM tbl_food";
+                            
+                            // Execute the Query
+                            $res = mysqli_query($conn, $sql);
 
-                    <tr>
-                        <td>3.</td>
-                        <td>Lionel Messi</td>
-                        <td>m10</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Food</a>
-                            <a href="#" class="btn-danger">Delete Food</a>
-                        </td>
-                    </tr>
+                            // Count  Rows to check whether we have foods or not
+                            $count = mysqli_num_rows($res);
 
+                            // Create Serial Number Variable and Set Default Value as 1
+                            $sn = 1;
+
+                            if($count > 0) {
+                                // We have food in Database
+                                // Get the Foods from Database and Display
+                                while($row=mysqli_fetch_assoc($res)) {
+                                    // Get the values from individual columns
+                                    $id  = $row['id'];
+                                    $title = $row['title'];
+                                    $price = $row['price'];
+                                    $image_name = $row['image_name'];
+                                    $featured = $row['featured'];
+                                    $active = $row['active'];
+                                    ?>
+
+                                    <tr>
+                                        <td><?php echo $sn++; ?></td>
+                                        <td><?php echo $title; ?></td>
+                                        <td>$<?php echo $price; ?></td>
+                                        <td>
+                                            <?php 
+                                                // Check whether we have image or not
+                                                if($image_name != "") {
+                                                    // We have Image, Display Image
+                                                    ?>
+                                                    <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" width="100px" >
+                                                    <?php
+                                                }else {
+                                                    // We do no have Image, Display the Error Message
+                                                    echo "<div class='error'>Image Not Added.</div>"; 
+                                                    
+                                                }
+                                               
+                                            ?>
+                                        </td>
+                                        <td><?php echo $featured; ?></td>
+                                        <td><?php echo $active; ?></td>
+                                        <td>
+                                            <a href="#" class="btn-secondary">Update Food</a>
+                                            <a href="#" class="btn-danger">Delete Food</a>
+                                        </td>
+                                    </tr>
+
+                                    <?php
+                                }
+
+                            }else {
+                                // Break the php to display the message
+                                ?>
+                                // Food not Added in Database
+                                <tr>
+                                    <td colspan='6'>
+                                        <div class='error'>Food not Added Yet.</div>
+                                    </td>
+                                </tr>
+                                <?php 
+                            }
+                        ?>        
                 </table>
     </div>
 </div>

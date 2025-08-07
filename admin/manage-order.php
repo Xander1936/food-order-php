b43fcd65a81eea868be56107743c1f9d38ab8375
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2025-08-07 18:00:10 +0100

    Display the Orders from the Database to the Admin Panel

--- admin/manage-order.php
+++ admin/manage-order.php
@@ -4,45 +4,79 @@
     <div class="wrapper">
         <h1>Manage Order</h1>
 
-        <br><br>
-                
+        <br><br><br>
+
                  <table class="tbl-full">
-                     <tr>
+                    <tr>
                          <th>S.N.</th>
-                         <th>Full Name</th>
-                         <th>Username</th>
+                         <th>Food</th>
+                         <th>Price</th>
+                         <th>Qty</th>
+                         <th>Total</th>
+                         <th>Order Date</th>
+                         <th>Status</th>
+                         <th>Customer Name</th>
+                         <th>Contact</th>
+                         <th>Email</th>
+                         <th>Address</th>
                          <th>Actions</th>
-                        </tr>
+                    </tr>
+
+                    <?php 
+                        // Get all the Orders from Database by Descending id
+                        $sql = "SELECT * FROM tbl_order ORDER BY id DESC"; // Display the Latest Order at First
+                        // Execute Query
+                        $res = mysqli_query($conn, $sql);
+                        // Count the Rows
+                        $count = mysqli_num_rows($res);
+
+                        $sn = 1; // Create a Serial Number and Set its initial value as 1
+
+                        if($count > 0) {
+                            // Order Available
+                            while($row=mysqli_fetch_assoc($res)){
+                                // Get all the Order Details
+                                $id = $row['id'];
+                                $food = $row['food'];
+                                $price = $row['price'];
+                                $qty = $row['qty'];
+                                $total = $row['total'];
+                                $order_date = $row['order_date'];
+                                $status = $row['status'];
+                                $customer_name = $row['customer_name'];
+                                $customer_contact = $row['customer_contact'];
+                                $customer_email = $row['customer_email'];
+                                $customer_address = $row['customer_address'];
+
+                                ?>
+
+                                    <tr>
+                                        <td><?php echo $sn++ ?></td>
+                                        <td><?php echo $food; ?></td>
+                                        <td><?php echo $price; ?></td>
+                                        <td><?php echo $qty; ?></td>
+                                        <td><?php echo $total; ?></td>
+                                        <td><?php echo $order_date; ?></td>
+                                        <td><?php echo $status; ?></td>
+                                        <td><?php echo $customer_name; ?></td>
+                                        <td><?php echo $customer_contact; ?></td>
+                                        <td><?php echo $customer_email; ?></td>
+                                        <td><?php echo $customer_address; ?></td>
+                                        <td>
+                                            <a href="#" class="btn-secondary">Update Order</a>
+                                        </td>
+                                    </tr>
+                                <?php
+                            }
+
+                        }else {
+                            // Order Not Available
+                            echo "<div class='error'>Orders Not Available</div>"; 
+                        }
+
+                    ?>
                         
-                        <tr>
-                            <td>1.</td>
-                            <td>Vijay Thapa</td>
-                            <td>vijaythapa</td>
-                            <td>
-                            <a href="#" class="btn-secondary">Update Order</a>
-                            <a href="#" class="btn-danger">Delete Order</a>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>2.</td>
-                        <td>Alexandre Massoda</td>
-                        <td>alex_xander</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Order</a>
-                            <a href="#" class="btn-danger">Delete Order</a>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>3.</td>
-                        <td>Lionel Messi</td>
-                        <td>m10</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Order</a>
-                            <a href="#" class="btn-danger">Delete Order</a>
-                        </td>
-                    </tr>
+                    
 
                 </table>
     </div>

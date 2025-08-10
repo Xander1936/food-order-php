5d8b47fdddc05482abe16fa2842645475ac35d96
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2025-08-10 13:37:06 +0100

    Security Homework comments on index.php

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,7 @@
 <?php include("partials/menu.php"); ?>
 
-        <?php 
+        <?php
+            // Security Homework: 
             // Use mysqli_real_escape_string() on all the website's inputs for the security to prevent attacks by SQL injection 
             // $input_name = mysqli_real_escape_string($conn, $_POST['input_name']);
         ?>
